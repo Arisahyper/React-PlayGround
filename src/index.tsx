2e8cd3c8fdd648314de853ef40500c71598d81c8
Aqua Minato <sweet4182691@gmail.com>
2021-07-16 20:17:47 +0900

    add: react-router-dom

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import App2 from './App2';
 import reportWebVitals from './reportWebVitals';
+import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <Router>
+        <Route exact path="/app" component={App} />
+        <Route path="/app2" component={App2} />
+      </Router>
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
